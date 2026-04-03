d7bfd2554269e7b653f92cb5d8e59607f860a7e4
wemppyy <denisfrostaliuk@gmail.com>
2026-04-03 20:50:52 +0200

    Refactor RecipesList component for improved readability

--- src/components/RecipesList/RecipesList.jsx
+++ src/components/RecipesList/RecipesList.jsx
@@ -1,26 +1,26 @@
-import { useState } from "react";
-import "./RecipesList.css";
+import './RecipesList.css'
 
 export default function RecipesList({
-    recipeList,
-    searchQuery = "",
-    itemsPerPage = 12,
-    page = 1,
+	recipeList,
+	searchQuery = '',
+	itemsPerPage = 12,
+	page = 1
 }) {
-    const startIndex = (page - 1) * itemsPerPage;
-    const endIndex = page * itemsPerPage;
+	const startIndex = (page - 1) * itemsPerPage
+	const endIndex = page * itemsPerPage
 
-    const recipesToRender = recipeList
-        .slice(startIndex, endIndex)
-        .map((recipe) => (
-            <div key={recipe.props.id} className="recipe_item">
-                {recipe}
-            </div>
-        ));
+	const recipesToRender = recipeList.slice(startIndex, endIndex).map(recipe => (
+		<div
+			key={recipe.props.id}
+			className="recipe_item"
+		>
+			{recipe}
+		</div>
+	))
 
-    return (
-        <>
-            <div className="recipes_list">{recipesToRender}</div>
-        </>
-    );
+	return (
+		<>
+			<div className="recipes_list">{recipesToRender}</div>
+		</>
+	)
 }
